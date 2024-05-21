e046dadcee139a15a89790276a55034cb67203a0
Dongwon <kdw686968@gmail.com>
2024-05-21 11:16:22 +0900

    server proxy 설정ㅞㅡ

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/Home.jsx";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/page",
+    element: <Home />,
+    children: [{ path: "write", element: <BoardWrite /> }],
+  },
+]);
 
 function App(props) {
   return (
-    <div></div>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
