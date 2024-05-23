b34bcd21c8553350a12f3a1d953cf59455ac7d5d
Dongwon <kdw686968@gmail.com>
2024-05-23 11:33:15 +0900

    별명 입력되지 않으면 저장버튼 비활성화

--- src/page/member/MemberEdit.jsx
+++ src/page/member/MemberEdit.jsx
@@ -62,6 +62,9 @@ export function MemberEdit() {
   if (member.password !== passwordCheck) {
     isDisabledSaveButton = true;
   }
+  if (member.nickName.trim().length === 0) {
+    isDisabledSaveButton = true;
+  }
 
   return (
     <Box>
@@ -99,7 +102,9 @@ export function MemberEdit() {
         <Box>
           <FormControl>별명</FormControl>
           <Input
-            onChange={(e) => setMember({ ...member, nickName: e.target.value })}
+            onChange={(e) =>
+              setMember({ ...member, nickName: e.target.value.trim() })
+            }
             value={member.nickName}
           />
         </Box>
