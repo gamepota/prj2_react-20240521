4fa687b59e4285d3be3746ced015080259643e64
Dongwon <kdw686968@gmail.com>
2024-05-23 11:18:01 +0900

    회원 정보 변경 시 기존 암호 확인

--- src/page/member/MemberEdit.jsx
+++ src/page/member/MemberEdit.jsx
@@ -5,6 +5,12 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   useDisclosure,
   useToast,
@@ -15,10 +21,12 @@ import { useNavigate, useParams } from "react-router-dom";
 
 export function MemberEdit() {
   const [member, setMember] = useState(null);
+  const [oldPassword, setOldPassword] = useState("");
   const { id } = useParams();
   const toast = useToast();
   const navigate = useNavigate();
   const { isOpen, onClose, onOpen } = useDisclosure();
+
   useEffect(() => {
     axios
       .get(`/api/member/${id}`)
@@ -29,7 +37,7 @@ export function MemberEdit() {
       .catch(() => {
         toast({
           status: "warning",
-          description: "회원 정보 조회 중 문제 발생",
+          description: "회원 정보 조회 중 문제가 발생하였습니다.",
           position: "top",
         });
         navigate("/");
@@ -38,7 +46,7 @@ export function MemberEdit() {
 
   function handleClickSave() {
     axios
-      .put("/api/member/modify", member)
+      .put("/api/member/modify", { ...member, oldPassword })
       .then((res) => {})
       .catch(() => {})
       .finally(() => {});
@@ -55,18 +63,7 @@ export function MemberEdit() {
         <Box>
           <FormControl>
             <FormLabel>이메일</FormLabel>
-            <Input value={member.email} readOnly />
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl>
-            <FormLabel>닉네임</FormLabel>
-            <Input
-              onChange={(e) =>
-                setMember({ ...member, nickName: e.target.value })
-              }
-              value={member.nickName}
-            />
+            <Input readOnly value={member.email} />
           </FormControl>
         </Box>
         <Box>
@@ -79,7 +76,7 @@ export function MemberEdit() {
               placeholder={"암호를 변경하려면 입력하세요"}
             />
             <FormHelperText>
-              입력하지 않으면 기존 암호가 유지됩니다.
+              입력하지 않으면 기존 암호를 변경하지 않습니다.
             </FormHelperText>
           </FormControl>
         </Box>
@@ -90,11 +87,36 @@ export function MemberEdit() {
           </FormControl>
         </Box>
         <Box>
-          <Button onClick={handleClickSave} colorScheme={"blue"}>
-            변경
+          <FormControl>별명</FormControl>
+          <Input
+            onChange={(e) => setMember({ ...member, nickName: e.target.value })}
+            value={member.nickName}
+          />
+        </Box>
+        <Box>
+          <Button onClick={onOpen} colorScheme={"blue"}>
+            저장
           </Button>
         </Box>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>기존 암호 확인</ModalHeader>
+          <ModalBody>
+            <FormControl>
+              <FormLabel>기존 암호</FormLabel>
+              <Input onChange={(e) => setOldPassword(e.target.value)} />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button colorScheme="blue" onClick={handleClickSave}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
