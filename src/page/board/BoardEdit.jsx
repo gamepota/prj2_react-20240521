c200d3c40d8c91574425b77ea4a774cded4c0729
Dongwon <kdw686968@gmail.com>
2024-05-24 11:11:23 +0900

    글 보기 화면의 수정/삭제 버튼 자신의 글만 보이기

--- src/page/board/BoardEdit.jsx
+++ src/page/board/BoardEdit.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
@@ -18,21 +18,35 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
+import { LoginContext } from "../../component/LoginProvider.jsx";
 
-export function BoardEdit() {
+export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const account = useContext(LoginContext);
   const toast = useToast();
   const navigate = useNavigate();
   const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
-    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
+    axios
+      .get(`/api/board/${id}`)
+      .then((res) => setBoard(res.data))
+      .catch((err) => {
+        if (err.response.status === 404) {
+          toast({
+            status: "info",
+            description: "해당 게시물이 존재하지 않습니다.",
+            position: "top",
+          });
+          navigate("/");
+        }
+      });
   }, []);
 
-  function handleClickSave() {
+  function handleClickRemove() {
     axios
-      .put("/api/board/edit", board, {
+      .delete(`/api/board/${id}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
@@ -40,19 +54,17 @@ export function BoardEdit() {
       .then(() => {
         toast({
           status: "success",
-          description: `${board.id}번 게시물이 수정되었습니다.`,
+          description: `${id}번 게시물이 삭제되었습니다.`,
           position: "top",
         });
-        navigate(`/board/${board.id}`);
+        navigate("/");
       })
-      .catch((err) => {
-        if (err.response.status === 400) {
-          toast({
-            status: "error",
-            description: `게시물이 수정되지 않았습니다. 작성한 내용을 확인해주세요.`,
-            position: "top",
-          });
-        }
+      .catch(() => {
+        toast({
+          status: "error",
+          description: `${id}번 게시물 삭제 중 오류가 발생하였습니다.`,
+          position: "top",
+        });
       })
       .finally(() => {
         onClose();
@@ -65,46 +77,51 @@ export function BoardEdit() {
 
   return (
     <Box>
-      <Box>{board.id}번 게시물 수정</Box>
+      <Box>{board.id}번 게시물</Box>
       <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input value={board.title} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea value={board.content} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input value={board.writer} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>작성일시</FormControl>
+        <Input type={"datetime-local"} value={board.inserted} readOnly />
+      </Box>
+      {account.hasAccess(board.memberId) && (
         <Box>
-          <FormControl>
-            <FormLabel>제목</FormLabel>
-            <Input
-              defaultValue={board.title}
-              onChange={(e) => setBoard({ ...board, title: e.target.value })}
-            />
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl>
-            <FormLabel>본문</FormLabel>
-            <Textarea
-              defaultValue={board.content}
-              onChange={(e) => setBoard({ ...board, content: e.target.value })}
-            ></Textarea>
-          </FormControl>
-        </Box>
-        <Box>
-          <FormControl>
-            <FormLabel>작성자</FormLabel>
-            <Input defaultValue={board.writer} readOnly />
-          </FormControl>
-        </Box>
-        <Box>
-          <Button colorScheme={"blue"} onClick={onOpen}>
-            저장
+          <Button
+            colorScheme={"purple"}
+            onClick={() => navigate(`/edit/${board.id}`)}
+          >
+            수정
+          </Button>
+          <Button colorScheme={"red"} onClick={onOpen}>
+            삭제
           </Button>
         </Box>
-      </Box>
+      )}
+
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader></ModalHeader>
-          <ModalBody>저장하시겠습니까?</ModalBody>
+          <ModalBody>삭제하시겠습니까?</ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>취소</Button>
-            <Button onClick={handleClickSave} colorScheme={"blue"}>
+            <Button colorScheme={"red"} onClick={handleClickRemove}>
               확인
             </Button>
           </ModalFooter>
