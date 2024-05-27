606d4bde6706466828a70cb8b4883f2e568f4465
Dongwon <kdw686968@gmail.com>
2024-05-27 12:21:20 +0900

    처음 버튼

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -58,9 +58,14 @@ export function BoardList() {
 
       <Box>
         {pageInfo.prevPageNumber && (
-          <Button onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}>
-            이전
-          </Button>
+          <>
+            <Button onClick={() => navigate(`/?page=1`)}>처음</Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}
+            >
+              이전
+            </Button>
+          </>
         )}
         {pageNumbers.map((pageNumber) => (
           <Button
