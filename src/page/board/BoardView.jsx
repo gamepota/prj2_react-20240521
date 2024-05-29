9b082b8cebd71d6dda76185cf8ee5e43ddfa4522
Dongwon <kdw686968@gmail.com>
2024-05-29 15:34:07 +0900

    좋아요 {like,count} 객체로 상태관리

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -87,17 +87,17 @@ export function BoardView() {
     return <Spinner />;
   }
 
+  function handleClickLike() {
+    axios.put(`/api/board/like`, { boardId: board.id });
+  }
+
   return (
     <Box>
       <Flex>
         <Heading>{board.id}번 게시물</Heading>
         <Spacer />
         <Flex>
-          <Box
-            onClick={() => setLike({ ...like, like: !like.like })}
-            cursor="pointer"
-            fontSize="3xl"
-          >
+          <Box onClick={handleClickLike} cursor="pointer" fontSize="3xl">
             {like.like && <FontAwesomeIcon icon={fullHeart} />}
             {like.like || <FontAwesomeIcon icon={emptyHeart} />}
           </Box>
