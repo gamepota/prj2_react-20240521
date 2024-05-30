0f038bd50baab94c8062f960db0d709f7375d8ca
Dongwon <kdw686968@gmail.com>
2024-05-30 12:42:18 +0900

    댓글 작성 후 댓글 리스트

--- src/page/component/comment/CommentComponent.jsx
+++ src/page/component/comment/CommentComponent.jsx
@@ -1,11 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import { CommentWrite } from "./CommentWrite.jsx";
 import { CommentList } from "./CommentList.jsx";
+import { useState } from "react";
 
 export function CommentComponent({ boardId }) {
+  const [isSending, setIsSending] = useState(false);
   return (
     <Box>
-      <CommentWrite boardId={boardId} />
+      <CommentWrite
+        boardId={boardId}
+        isSending={isSending}
+        setIsSending={setIsSending}
+      />
       <CommentList boardId={boardId} />
     </Box>
   );
