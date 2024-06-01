b8ef8dbaa51d605cf1abc9eac713a23fcf0f696a
Dongwon <kdw686968@gmail.com>
2024-06-01 14:46:10 +0900

    글쓰기 css

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -1,10 +1,17 @@
 import {
   Box,
   Button,
+  Card,
+  CardBody,
+  CardHeader,
   FormControl,
   FormHelperText,
   FormLabel,
+  Heading,
   Input,
+  Stack,
+  StackDivider,
+  Text,
   Textarea,
   useToast,
 } from "@chakra-ui/react";
@@ -60,49 +67,69 @@ export function BoardWrite() {
     disableSaveButton = true;
   }
 
-  //file 목록 작성
+  // file 목록 작성
   const fileNameList = [];
   for (let i = 0; i < files.length; i++) {
-    fileNameList.push(<li>{files[i].name}</li>);
+    fileNameList.push(
+      <Box>
+        <Text fontSize={"md"}>{files[i].name}</Text>
+      </Box>,
+    );
   }
+
   return (
     <Box>
-      <Box>글 작성 화면</Box>
+      <Box mb={10}>
+        <Heading>글 작성</Heading>
+      </Box>
       <Box>
-        <Box>
+        <Box mb={7}>
           <FormControl>
             <FormLabel>제목</FormLabel>
             <Input onChange={(e) => setTitle(e.target.value)} />
           </FormControl>
         </Box>
-        <Box>
+        <Box mb={7}>
           <FormControl>
             <FormLabel>본문</FormLabel>
             <Textarea onChange={(e) => setContent(e.target.value)} />
           </FormControl>
         </Box>
-        <Box>
+        <Box mb={7}>
           <FormControl>
             <FormLabel>파일</FormLabel>
             <Input
-              multiple="true"
+              multiple
               type="file"
               accept="image/*"
               onChange={(e) => setFiles(e.target.files)}
             />
-            <FormHelperText>총용량 10MB, 개당 1MB</FormHelperText>
+            <FormHelperText>
+              총 용량은 10MB, 한 파일은 1MB를 초과할 수 없습니다.
+            </FormHelperText>
           </FormControl>
         </Box>
-        <Box>
-          <ul>{fileNameList}</ul>
-        </Box>
-        <Box>
+        {fileNameList.length > 0 && (
+          <Box mb={7}>
+            <Card>
+              <CardHeader>
+                <Heading size="md">선택된 파일 목록</Heading>
+              </CardHeader>
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing={4}>
+                  {fileNameList}
+                </Stack>
+              </CardBody>
+            </Card>
+          </Box>
+        )}
+        <Box mb={7}>
           <FormControl>
             <FormLabel>작성자</FormLabel>
             <Input readOnly value={account.nickName} />
           </FormControl>
         </Box>
-        <Box>
+        <Box mb={7}>
           <Button
             isLoading={loading}
             isDisabled={disableSaveButton}
